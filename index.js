a880c97a7abe51daf1bad910896977fa840cc734
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-06-19 20:04:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola mundo desde Express en Render!');
+  res.send('¡Hola mundo!');
 });
 
 app.listen(PORT, () => {
